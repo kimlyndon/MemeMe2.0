b8e37362070f99a7145d82b7ade5f83ef2f9c5f4
Kim Lyndon <codeCulture101@gmail.com>
2018-06-06 14:15:36 -0400

    update: Add swipe to delete to table view

--- MemeMe2.0/ViewControllers/SentMemesTableViewController.swift
+++ MemeMe2.0/ViewControllers/SentMemesTableViewController.swift
@@ -42,6 +42,10 @@ class SentMemesTableViewController: UITableViewController {
         self.navigationController?.pushViewController(memeDetailController, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             memes?.remove(at: indexPath.row)
