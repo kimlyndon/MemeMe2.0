8650aef2d3423993e3c3077b149f18a6c428da19
Kim Lyndon <codeCulture101@gmail.com>
2018-06-06 14:16:10 -0400

    update: Add edit function to detail view

--- MemeMe2.0/ViewControllers/MemeDetailViewController.swift
+++ MemeMe2.0/ViewControllers/MemeDetailViewController.swift
@@ -14,16 +14,32 @@ class MemeDetailViewController: UIViewController {
     
     @IBOutlet weak var sentMemeDetailImageView: UIImageView!
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        sentMemeDetailImageView.image = memes.memedImage
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.edit, target: self, action: #selector(MemeDetailViewController.startMemeEditor))
+        sentMemeDetailImageView.image = memes.memedImage
+        self.tabBarController?.tabBar.isHidden = true
+    
+}
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.tabBarController?.tabBar.isHidden = false
         
     }
-}
+    @objc func startMemeEditor() {
+        let memeEditorVC = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        
+        self.present(memeEditorVC, animated: true, completion: nil)
+        
+        memeEditorVC.topTextField.text = memes.topText
+        memeEditorVC.bottomTextField.text = memes.bottomText
+        memeEditorVC.imagePickerView.image = memes.originalImage
+        memeEditorVC.isEditing = true
+    }
+    }
+
